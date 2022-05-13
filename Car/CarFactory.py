a0f97303296b5194164201119cd610eabec9f2a9
Federico Cirina <effedeso@gmail.com>
2022-05-13 13:17:51 +0100

    Deleted old car classes, CarFactory completed

--- Car/CarFactory.py
+++ Car/CarFactory.py
@@ -1,8 +1,9 @@
 from abc import ABC
 from datetime import date
 from Battery.SpindlerBattery import SpindlerBattery
+from Battery.NubbinBattery import NubbinBattery
 import Car
-from engine.CapuletEngine import CapuletEngine
+from engine import *
 
 class CarFactory(ABC):
 
@@ -10,18 +11,46 @@ class CarFactory(ABC):
         super().__init__()
 
     def create_calliope(current_date : date, last_service_date : date, current_mileage : int, last_service_mileage : int):
-        engine = CapuletEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+        engine = engine.CapuletEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
 
         battery = SpindlerBattery(last_service_date=last_service_date, current_date=current_date)
+
+        car = Car(engine=engine, battery=battery)
+
+        return car
     
     def create_glissade(current_date : date, last_service_date : date, current_mileage : int, last_service_mileage : int):
-        return Car
+        engine = engine.WilloughbyEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+
+        battery = SpindlerBattery(last_service_date=last_service_date, current_date=current_date)
+
+        car = Car(engine=engine, battery=battery)
+
+        return car
 
     def create_palindrome(current_date : date, last_service_date : date, current_mileage : int, last_service_mileage : int):
-        return Car
+        engine = engine.SternamEngine(False)
+
+        battery = NubbinBattery(last_service_date=last_service_date, current_date=current_date)
+
+        car = Car(engine=engine, battery=battery)
+
+        return car
 
     def create_thovax(current_date : date, last_service_date : date, current_mileage : int, last_service_mileage : int):
-        return Car
+        engine = engine.CapuletEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+
+        battery = NubbinBattery(last_service_date=last_service_date, current_date=current_date)
+
+        car = Car(engine=engine, battery=battery)
+
+        return car
 
     def create_rorschach(current_date : date, last_service_date : date, current_mileage : int, last_service_mileage : int):
-        return Car
+        engine = engine.WilloughbyEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+
+        battery = NubbinBattery(last_service_date=last_service_date, current_date=current_date)
+
+        car = Car(engine=engine, battery=battery)
+
+        return car
